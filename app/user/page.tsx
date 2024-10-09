34ef9c3356e2bf63b12842d1fc1514409fd9951d
Matt Ellis <matt@brightsignals.co.uk>
2024-10-09 10:31:33 +0100

    ADDED: login and supabase integration

---
+++ app/user/page.tsx
@@ -0,0 +1,11 @@
+import UserProfile from "./UserProfile";
+
+export default function Login() {
+    return (
+        <div>
+            <h1 className="text-7xl">Login</h1>
+
+            <UserProfile />
+        </div>
+    );
+}
